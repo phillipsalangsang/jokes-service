6fac6eaef68a76633ffc189a3bc495fdb0d66945
phillipsalangsang <phillip_salangsang@hotmail.com>
2023-04-05 11:57:34 +0100

    Workshop task done with extension

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
 const express = require('express');
 const app = express();
-const { Joke } = require('./db');
+const { Joke, Op } = require('./db');
 
+// app.use(express.json());
+app.set("json spaces", "\t");
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+// app.use(express.urlencoded({extended:true}));
 
 app.get('/jokes', async (req, res, next) => {
+  if (req.query) {
+    try {
+      const jokes = await Joke.findAll({
+        where: {
+            tags: {
+              [Op.substring]: req.query.tags ?? "",
+            },
+            joke: {
+              [Op.substring]: req.query.content ?? "",
+            }
+        }
+      });
+      res.send(jokes);
+    } catch (error) {
+      console.error(error);
+      next(error)
+    }
+    return;
+  }
+  
   try {
     // TODO - filter the jokes by tags and content
-    const jokes = [];
+    const jokes = await Joke.findAll();
     res.send(jokes);
   } catch (error) {
     console.error(error);
     next(error)
   }
 });
+
+app.post("/jokes", async (req, res, next) => {
+  try {
+      const joke = await Joke.create(req.body);
+      res.status(201).send();
+  } catch (error) {
+      console.error(error);
+      next(error);
+  }
+})
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
